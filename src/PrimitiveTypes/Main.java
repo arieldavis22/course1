456544d773293c0ab6314dd3e13bfee43dd7f0ee
arieldavis22 <amanichai100@gmail.com>
2022-07-07 15:50:56 -0400

    short byte long

--- src/PrimitiveTypes/Main.java
+++ src/PrimitiveTypes/Main.java
@@ -12,8 +12,9 @@ public class Main {
         int myMinIntVal = Integer.MIN_VALUE;
         int myMaxIntVal = Integer.MAX_VALUE;
         // The variables following the min and max string literals are converted into a string
-        System.out.println("Min: " + myMinIntVal);
-        System.out.println("Max: " + myMaxIntVal);
+        // In occupies 32 bits and has a width of 32.
+        System.out.println("Min Int: " + myMinIntVal);
+        System.out.println("Max Int: " + myMaxIntVal);
 
         /*
          Overflow is caused when we try to put to large a number
@@ -23,5 +24,36 @@ public class Main {
          */
         System.out.println("Overflow: " + (myMaxIntVal + 1));
         System.out.println("Underflow: " + (myMinIntVal - 1));
+
+        // A Byte occupies 8 bits. Which means it has a width of 8.
+        byte myMinByteVal = Byte.MIN_VALUE;
+        byte myMaxByteVal = Byte.MAX_VALUE;
+        System.out.println("Min Byte: " + myMinByteVal);
+        System.out.println("Max Byte: " + myMaxByteVal);
+
+        // A short occupies 16 bits, and has a width of 16.
+        short myMinShortVal = Short.MIN_VALUE;
+        short myMaxShortVal = Short.MAX_VALUE;
+        System.out.println("Min Short: " + myMinShortVal);
+        System.out.println("Max Short: " + myMaxShortVal);
+
+        // A long occupies 64 bits, and has a width of 64
+        // 2^63
+        long myLongVal = 100L;
+        long myMinLongVal = Long.MIN_VALUE;
+        long myMaxLongVal = Long.MAX_VALUE;
+        System.out.println("Min Long: " + myMinLongVal);
+        System.out.println("Max Long: " + myMaxLongVal);
+
+        int myTotal = (myMinIntVal / 2);
+        // Casting in Java means to treat or convert a number from one type to another
+        byte myNewByteVal = (byte) (myMinByteVal / 2);
+
+        // Challenge
+        byte aByte = 2;
+        short aShort = 2;
+        int anInt = 3;
+        long aLong = 50_000L + (10 * (aByte + aShort + anInt));
+        System.out.println(aLong);
     }
 }
