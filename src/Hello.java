e742f0bb68b40dd9f3d38da7342f9bbcd13c5e1d
arieldavis22 <amanichai100@gmail.com>
2022-07-07 13:48:02 -0400

    init. class hello

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+/*
+The public Java keyword is called an access modifier. Allows us to define scope
+Keyword class is used to define a class, followed by the name of the class.
+ */
+public class Hello {
+
+}
