4272bccfbb0b11829edc7f7ada042bc3fd309b20
arieldavis22 <amanichai100@gmail.com>
2022-07-07 13:59:59 -0400

    hello world

--- src/Hello.java
+++ src/Hello.java
@@ -4,4 +4,13 @@ Keyword class is used to define a class, followed by the name of the class.
  */
 public class Hello {
 
+    /*
+    Java will look for a main method when running a program. it is the entry point to any Java code
+    Public is the same access modifier.
+    Void is the keyword used to tell java that this method will not return anything.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
