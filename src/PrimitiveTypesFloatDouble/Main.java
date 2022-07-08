a5db4b4d3caf9d708070313d6ecb9ba7a0716905
arieldavis22 <amanichai100@gmail.com>
2022-07-08 12:26:26 -0400

    float double

---
+++ src/PrimitiveTypesFloatDouble/Main.java
@@ -0,0 +1,9 @@
+package PrimitiveTypesFloatDouble;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Floating point numbers are also known as real numbers.
+
+    }
+}
