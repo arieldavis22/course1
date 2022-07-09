54e818afcdba278ada880ddaa1e3eeb61bd80d71
arieldavis22 <amanichai100@gmail.com>
2022-07-09 13:09:38 -0400

    if challenge

--- src/OperatorsExpressions/Main.java
+++ src/OperatorsExpressions/Main.java
@@ -40,8 +40,22 @@ public class Main {
         }
 
         int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100) {
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("yay");
+        }
+
+        // Challenge
+
+        double aDouble = 20d;
+        double secondDouble = 80d;
+
+        double theResult = (100 * (aDouble + secondDouble)) % 40d;
+        System.out.println(theResult);
+
+        boolean isTrue = (theResult == 0) ? true : false;
+        System.out.println(isTrue);
+        if (!isTrue) {
+            System.out.println("remainder");
         }
     }
 }
